b7ce5cc9a367254753fdc98c1a4af7ccec7e537b
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-07-03 10:05:00 -0300

    260703-01

--- tests/__init__.py
+++ tests/__init__.py
@@ -25,39 +25,39 @@ if TYPE_CHECKING:
         x as x,
     )
 
-    from tests.base import (
+    from flext_dbt_oracle_wms.tests.base import (
         TestsFlextDbtOracleWmsServiceBase as TestsFlextDbtOracleWmsServiceBase,
         s as s,
     )
-    from tests.constants import (
+    from flext_dbt_oracle_wms.tests.constants import (
         TestsFlextDbtOracleWmsConstants as TestsFlextDbtOracleWmsConstants,
         c as c,
     )
-    from tests.models import (
+    from flext_dbt_oracle_wms.tests.models import (
         TestsFlextDbtOracleWmsModels as TestsFlextDbtOracleWmsModels,
         m as m,
     )
-    from tests.protocols import (
+    from flext_dbt_oracle_wms.tests.protocols import (
         TestsFlextDbtOracleWmsProtocols as TestsFlextDbtOracleWmsProtocols,
         p as p,
     )
-    from tests.settings import (
+    from flext_dbt_oracle_wms.tests.settings import (
         TestsFlextDbtOracleWmsSettings as TestsFlextDbtOracleWmsSettings,
     )
-    from tests.typings import (
+    from flext_dbt_oracle_wms.tests.typings import (
         TestsFlextDbtOracleWmsTypes as TestsFlextDbtOracleWmsTypes,
         t as t,
     )
-    from tests.unit.test_cli import (
+    from flext_dbt_oracle_wms.tests.unit.test_cli import (
         TestsFlextDbtOracleWmsCli as TestsFlextDbtOracleWmsCli,
     )
-    from tests.unit.test_module_governance import (
+    from flext_dbt_oracle_wms.tests.unit.test_module_governance import (
         TestsFlextDbtOracleWmsModuleGovernance as TestsFlextDbtOracleWmsModuleGovernance,
     )
-    from tests.unit.test_simple_api import (
+    from flext_dbt_oracle_wms.tests.unit.test_simple_api import (
         TestsFlextDbtOracleWmsSimpleApi as TestsFlextDbtOracleWmsSimpleApi,
     )
-    from tests.utilities import (
+    from flext_dbt_oracle_wms.tests.utilities import (
         TestsFlextDbtOracleWmsUtilities as TestsFlextDbtOracleWmsUtilities,
         u as u,
     )
--- tests/unit/__init__.py
+++ tests/unit/__init__.py
@@ -3,31 +3,25 @@
 
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
+if TYPE_CHECKING:
+    from flext_dbt_oracle_wms.tests.unit.test_cli import (
+        TestsFlextDbtOracleWmsCli as TestsFlextDbtOracleWmsCli,
+    )
+    from flext_dbt_oracle_wms.tests.unit.test_module_governance import (
+        TestsFlextDbtOracleWmsModuleGovernance as TestsFlextDbtOracleWmsModuleGovernance,
+    )
+    from flext_dbt_oracle_wms.tests.unit.test_simple_api import (
+        TestsFlextDbtOracleWmsSimpleApi as TestsFlextDbtOracleWmsSimpleApi,
+    )
 _LAZY_IMPORTS = build_lazy_import_map(
     {
         ".test_cli": ("TestsFlextDbtOracleWmsCli",),
         ".test_module_governance": ("TestsFlextDbtOracleWmsModuleGovernance",),
         ".test_simple_api": ("TestsFlextDbtOracleWmsSimpleApi",),
-        "flext_tests": (
-            "c",
-            "d",
-            "e",
-            "h",
-            "m",
-            "p",
-            "r",
-            "s",
-            "t",
-            "td",
-            "tf",
-            "tk",
-            "tm",
-            "tv",
-            "u",
-            "x",
-        ),
     },
 )
 
