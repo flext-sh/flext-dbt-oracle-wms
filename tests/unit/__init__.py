510b861afc6a13f3296ed60fbad0fedbe367fc1f
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-05 15:49:15 -0300

    [WIP] refactor: checkpoint fleet conformance

--- tests/unit/__init__.py
+++ tests/unit/__init__.py
@@ -3,24 +3,16 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
 from types import MappingProxyType
+from typing import TYPE_CHECKING
 
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 if TYPE_CHECKING:
     from flext_tests import c, d, e, h, m, p, r, s, t, td, tf, tk, tm, tv, u, x
 
-    from .test_api import TestsFlextDbtOracleWmsApi
-    from .test_cli import TestsFlextDbtOracleWmsCli
-    from .test_connection_profile import (
-        test_connection_profile_returns_typed_oracle_wms_wire_shape,
-    )
     from .test_module_governance import TestsFlextDbtOracleWmsModuleGovernance
 __all__: tuple[str, ...] = (
-    "TestsFlextDbtOracleWmsApi",
-    "TestsFlextDbtOracleWmsCli",
     "TestsFlextDbtOracleWmsModuleGovernance",
     "c",
     "d",
@@ -32,7 +24,6 @@ __all__: tuple[str, ...] = (
     "s",
     "t",
     "td",
-    "test_connection_profile_returns_typed_oracle_wms_wire_shape",
     "tf",
     "tk",
     "tm",
@@ -44,11 +35,6 @@ __all__: tuple[str, ...] = (
 _LAZY_IMPORTS = MappingProxyType(
     build_lazy_import_map(
         MappingProxyType({
-            ".test_api": ("TestsFlextDbtOracleWmsApi",),
-            ".test_cli": ("TestsFlextDbtOracleWmsCli",),
-            ".test_connection_profile": (
-                "test_connection_profile_returns_typed_oracle_wms_wire_shape",
-            ),
             ".test_module_governance": ("TestsFlextDbtOracleWmsModuleGovernance",),
             "flext_tests": (
                 "c",
