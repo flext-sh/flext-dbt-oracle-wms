fbcec9de1caab9325fda25219144e38e89414c25
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-22 12:53:30 -0300

    merge: stabilize branch against 0.12.0-dev tip + converge gen

--- src/flext_dbt_oracle_wms/__init__.py
+++ src/flext_dbt_oracle_wms/__init__.py
@@ -24,21 +24,22 @@ if TYPE_CHECKING:
     from flext_oracle_wms import e
 
     from . import services
+    from .__version__ import FlextDbtOracleWmsVersion
     from ._config import FlextDbtOracleWmsConfig, config
     from ._settings import FlextDbtOracleWmsSettings, settings
     from .api import FlextDbtOracleWms, dbt_oracle_wms
     from .base import FlextDbtOracleWmsServiceBase, FlextDbtOracleWmsServiceBase as s
     from .cli import FlextDbtOracleWmsCliService, main
-    from .constants import FlextDbtOracleWmsConstants, c
-    from .models import FlextDbtOracleWmsModels, m
-    from .protocols import FlextDbtOracleWmsProtocols, p
+    from .constants import FlextDbtOracleWmsConstants, FlextDbtOracleWmsConstants as c
+    from .models import FlextDbtOracleWmsModels, FlextDbtOracleWmsModels as m
+    from .protocols import FlextDbtOracleWmsProtocols, FlextDbtOracleWmsProtocols as p
     from .services.base import FlextDbtOracleWmsBase
     from .services.client import FlextDbtOracleWmsClient
     from .services.generation import FlextDbtOracleWmsGeneration
     from .services.metadata import FlextDbtOracleWmsMetadata
     from .services.workflow import FlextDbtOracleWmsWorkflow
-    from .typings import FlextDbtOracleWmsTypes, t
-    from .utilities import FlextDbtOracleWmsUtilities, u
+    from .typings import FlextDbtOracleWmsTypes, FlextDbtOracleWmsTypes as t
+    from .utilities import FlextDbtOracleWmsUtilities, FlextDbtOracleWmsUtilities as u
 __all__: tuple[str, ...] = (
     "FlextDbtOracleWms",
     "FlextDbtOracleWmsBase",
@@ -54,6 +55,7 @@ __all__: tuple[str, ...] = (
     "FlextDbtOracleWmsSettings",
     "FlextDbtOracleWmsTypes",
     "FlextDbtOracleWmsUtilities",
+    "FlextDbtOracleWmsVersion",
     "FlextDbtOracleWmsWorkflow",
     "__author__",
     "__author_email__",
@@ -63,37 +65,28 @@ __all__: tuple[str, ...] = (
     "__url__",
     "__version__",
     "__version_info__",
-    "api",
     "c",
-    "cli",
     "config",
-    "core",
     "d",
     "dbt_oracle_wms",
     "e",
-    "from_json",
     "h",
-    "lazy_attribute",
     "m",
     "main",
-    "meltano",
-    "oracle_wms",
     "p",
     "r",
     "s",
     "services",
     "settings",
     "t",
-    "to_json",
-    "to_jsonable_python",
     "u",
-    "web",
     "x",
 )
 
 _LAZY_IMPORTS = MappingProxyType(
     build_lazy_import_map(
         MappingProxyType({
+            ".__version__": ("FlextDbtOracleWmsVersion",),
             "._config": ("FlextDbtOracleWmsConfig", "config"),
             "._settings": ("FlextDbtOracleWmsSettings", "settings"),
             ".api": ("FlextDbtOracleWms", "dbt_oracle_wms"),
